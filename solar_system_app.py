eb6d216e36eba863bd90d1c1ec2b85f1b90ea837
Ciaran <ciaranmckay01@gmail.com>
2023-12-04 08:56:25 -0500

    quiz feature added

--- solar_system_app.py
+++ solar_system_app.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QWidget, QPushButton, QHBoxLayout, QMessageBox, QFormLayout, QLineEdit, QDialogButtonBox, QDialog, QLabel, QComboBox
+from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QWidget, QPushButton, QHBoxLayout, QMessageBox, QFormLayout, QLineEdit, QDialogButtonBox, QDialog, QLabel, QComboBox, QRadioButton
 from PyQt5.QtCore import QTimer
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
@@ -15,40 +15,34 @@ class SolarSystemApp(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
+        self.quiz_progress = 0
+        self.initializeQuizQuestions()
+
 
     def initUI(self):
         self.setWindowTitle('Solar System Simulation')
         self.setGeometry(100, 100, 800, 600)
-
         self.central_widget = QWidget(self)
         self.setCentralWidget(self.central_widget)
         layout = QVBoxLayout(self.central_widget)
-
+        
         self.canvas = FigureCanvas(Figure())
         self.ax = self.canvas.figure.add_subplot(111, projection='3d')
         layout.addWidget(self.canvas)
 
-        self.startButton = QPushButton('Start Simulation', self)
-        self.startButton.clicked.connect(self.startSimulation)
-        controlsLayout = QHBoxLayout()
-        controlsLayout.addWidget(self.startButton)
-        layout.addLayout(controlsLayout)
+        # Create menu bar items
+        menuBar = self.menuBar()
+        controlMenu = menuBar.addMenu('Controls')
+        quizMenu = menuBar.addMenu('Quiz')
 
-        self.toggleTrailButton = QPushButton('Toggle Orbit Trails', self)
-        self.toggleTrailButton.clicked.connect(self.toggleOrbitTrails)
-        controlsLayout.addWidget(self.toggleTrailButton)
+        # Add actions to the control menu
+        controlMenu.addAction('Start Simulation', self.startSimulation)
+        controlMenu.addAction('Toggle Orbit Trails', self.toggleOrbitTrails)
+        controlMenu.addAction('List Bodies Info', self.listBodiesInfo)
+        controlMenu.addAction('Add New Body', self.showNewBodyDialog)
 
-        self.listBodiesButton = QPushButton('List Bodies Info', self)
-        self.listBodiesButton.clicked.connect(self.listBodiesInfo)
-        controlsLayout.addWidget(self.listBodiesButton)
-
-        #self.stableOrbitButton = QPushButton('Stable Orbit Calculator', self)
-        #self.stableOrbitButton.clicked.connect(self.showStableOrbitDialog)
-        #controlsLayout.addWidget(self.stableOrbitButton)
-
-        self.addBodyButton = QPushButton('Add New Body', self)
-        self.addBodyButton.clicked.connect(self.showNewBodyDialog)
-        controlsLayout.addWidget(self.addBodyButton)
+        # Add action to the quiz menu
+        quizMenu.addAction('Start Quiz', self.showQuiz)
 
         self.initSolarSystem()
     
@@ -124,7 +118,121 @@ class SolarSystemApp(QMainWindow):
         if reply == QMessageBox.Yes:
             # Assuming initial position in the XY plane for simplicity
             self.addNewBody(mass, massUnit, f"{distance_components[0]},{distance_components[1]},0", f"0,0,{suggested_velocity}")
+
+    def initializeQuizQuestions(self):
+        self.quiz_questions = [
+            # Include all the quiz questions here with explanations
+            {"question": "What is the force that keeps planets in orbit around the sun?", 
+             "options": ["Gravity", "Magnetism", "Electrostatic force"], 
+             "answer": "Gravity",
+             "explanation": "Gravity is the force that keeps planets in orbit. It's the attractive force between two masses."},
+
+            {"question": "If a satellite orbits closer to the Earth, how does its orbital speed compare to a satellite farther away?",
+             "options": ["Faster", "Slower", "The same", "Cannot be determined"],
+             "answer": "Faster",
+             "explanation": "According to Kepler's third law, orbital speed increases as the orbital radius decreases."},
+            
+            {"question": "How does the gravitational force between two objects change if the distance between them doubles?",
+             "options": ["Increases four times", "Decreases four times", "Increases two times", "Decreases two times"],
+             "answer": "Decreases four times",
+             "explanation": "Gravitational force is inversely proportional to the square of the distance. Doubling the distance decreases the force by a factor of four."},
+
+            {"question": "What is the minimum velocity needed for an object to maintain a stable orbit around the Sun at 1 AU?",
+            "options": ["29.78 km/s", "24.13 km/s", "42.1 km/s", "33.2 km/s"],
+            "answer": "29.78 km/s",
+            "explanation": "The minimum velocity for a stable orbit at 1 AU (the distance of Earth from the Sun) is about 29.78 km/s. This is calculated using the formula v = √(GM/r), where G is the gravitational constant, M is the mass of the Sun, and r is the radius of the orbit."},
+
+             # New questions
+            {"question": "What would happen to Earth's orbit if the Sun's mass suddenly doubled?",
+             "options": ["The orbit would remain the same", "The orbit would become more elliptical", "The Earth would move closer to the Sun", "The Earth would move farther from the Sun"],
+             "answer": "The Earth would move closer to the Sun",
+             "explanation": "Doubling the Sun's mass would increase the gravitational force, pulling Earth closer and possibly leading to a more elliptical orbit."},
+
+            {"question": "What is the escape velocity from Earth's surface?",
+             "options": ["7.9 km/s", "11.2 km/s", "9.8 m/s^2", "5.5 km/s"],
+             "answer": "11.2 km/s",
+             "explanation": "Escape velocity is the speed needed to break free from a planet's gravitational pull. For Earth, it's approximately 11.2 km/s."},
+
+            {"question": "How does the orbital period of a planet change with respect to its semi-major axis length?",
+             "options": ["Increases linearly", "Decreases linearly", "Increases with the square of the semi-major axis length", "Increases with the cube root of the semi-major axis length"],
+             "answer": "Increases with the cube root of the semi-major axis length",
+             "explanation": "According to Kepler's third law, the square of a planet's orbital period is proportional to the cube of the semi-major axis of its orbit."},
+
+            {"question": "If a new planet was discovered at 2 AU from the Sun, how would its year compare to Earth's?",
+             "options": ["About the same as Earth's", "Twice as long as Earth's", "Half as long as Earth's", "Approximately 2.8 times longer than Earth's"],
+             "answer": "Approximately 2.8 times longer than Earth's",
+             "explanation": "Using Kepler's third law, a planet at 2 AU would have an orbital period √2³ = 2.8 times longer than Earth's year."},
+
+            {"question": "What is the significance of the Lagrange points in a two-body system?",
+             "options": ["Points where gravitational forces are balanced", "Points where escape velocity is zero", "Points where orbital speed is highest", "Points where gravitational forces are strongest"],
+             "answer": "Points where gravitational forces are balanced",
+             "explanation": "Lagrange points are positions where the gravitational pull of two large bodies precisely equals the centripetal force required to orbit with them."},
+
+            {"question": "Calculate the orbital speed of a satellite at a height of 300 km above Earth's surface. (Earth's radius = 6371 km, Earth's mass = 5.97e24 kg)",
+             "options": ["7.8 km/s", "8.1 km/s", "7.4 km/s", "8.5 km/s"],
+             "answer": "7.8 km/s",
+             "explanation": "Orbital speed v = √(GM/(R+h)), where R is Earth's radius, h is satellite's height. Plugging in the values gives v ≈ 7.8 km/s."},
+
+            {"question": "A planet orbits a star twice the mass of the Sun at the same distance as Earth from the Sun. What is its orbital period compared to Earth's?",
+             "options": ["Same as Earth's", "Twice Earth's", "Half of Earth's", "Four times Earth's"],
+             "answer": "Same as Earth's",
+             "explanation": "Orbital period depends on the semi-major axis, not the star's mass. So, it remains the same as Earth's."},
+
+            {"question": "What would be the gravitational force between two 1 kg masses 1 meter apart?",
+             "options": ["6.67e-11 N", "6.67e-10 N", "1 N", "0 N"],
+             "answer": "6.67e-11 N",
+             "explanation": "Using Newton's law of universal gravitation, F = Gm1m2/r², where G is 6.67e-11 Nm²/kg²."},
+
+        ]
     
+        self.quiz_progress = 0
+
+    def showQuiz(self):
+        if self.quiz_progress >= len(self.quiz_questions):
+            QMessageBox.information(self, "Quiz", "You have completed all the questions!")
+            choice = QMessageBox.question(self, "Restart Quiz?", "Do you want to restart the quiz?", QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+            if choice == QMessageBox.Yes:
+                self.quiz_progress = 0
+            return
+
+        self.current_question = self.quiz_questions[self.quiz_progress]
+        dialog = QDialog(self)
+        dialog.setWindowTitle("Quiz")
+
+        layout = QVBoxLayout(dialog)
+
+        questionLabel = QLabel(self.current_question["question"], dialog)
+        layout.addWidget(questionLabel)
+
+        self.selected_answer = None
+        for option in self.current_question["options"]:
+            rb = QRadioButton(option, dialog)
+            rb.toggled.connect(lambda checked, opt=option: self.onAnswerSelected(checked, opt))
+            layout.addWidget(rb)
+
+        submitButton = QPushButton("Submit", dialog)
+        submitButton.clicked.connect(lambda: self.checkAnswer(dialog, submitButton))
+        layout.addWidget(submitButton)
+
+        dialog.setLayout(layout)
+        dialog.exec_()
+
+    def checkAnswer(self, dialog, submitButton):
+        correct = self.selected_answer == self.current_question["answer"]
+        message = "Correct!" if correct else f"Incorrect! The correct answer was '{self.current_question['answer']}'.\n\nExplanation: {self.current_question['explanation']}"
+        QMessageBox.information(self, "Quiz", message)
+        submitButton.setText("Next Question" if self.quiz_progress < len(self.quiz_questions) - 1 else "Finish Quiz")
+        submitButton.clicked.disconnect()
+        submitButton.clicked.connect(lambda: self.nextOrFinishQuiz(dialog))
+
+    def nextOrFinishQuiz(self, dialog):
+        self.quiz_progress += 1
+        dialog.accept()
+        self.showQuiz()
+
+    def onAnswerSelected(self, checked, option):
+        if checked:
+            self.selected_answer = option
 
     def showInfo(self):
         infoDialog = QDialog(self)
